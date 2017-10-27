5d3e825b72eb552877239e0628526ede3e8cfe5d
USER-PC\USER <qlr12qlyhw@gmail.com>
2017-10-27 12:36:14 +0900

    DB

---
+++ 기초데이터/tb_record.sql
@@ -0,0 +1,11 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-10월-27-2017   
+--------------------------------------------------------
+REM INSERTING into HR.TB_RECORD
+SET DEFINE OFF;
+Insert into HR.TB_RECORD (RECORD_NO,ID,TEST_CODE,SCORE,RECORD_DATE) values (13,'admin',6,10,to_date('17/10/27','RR/MM/DD'));
+Insert into HR.TB_RECORD (RECORD_NO,ID,TEST_CODE,SCORE,RECORD_DATE) values (1,'aaa',6,100,to_date('17/10/27','RR/MM/DD'));
+Insert into HR.TB_RECORD (RECORD_NO,ID,TEST_CODE,SCORE,RECORD_DATE) values (8,'admin',6,0,to_date('17/10/27','RR/MM/DD'));
+Insert into HR.TB_RECORD (RECORD_NO,ID,TEST_CODE,SCORE,RECORD_DATE) values (11,'admin',6,0,to_date('17/10/27','RR/MM/DD'));
+Insert into HR.TB_RECORD (RECORD_NO,ID,TEST_CODE,SCORE,RECORD_DATE) values (14,'wangdeapo1',6,20,to_date('17/10/27','RR/MM/DD'));
+Insert into HR.TB_RECORD (RECORD_NO,ID,TEST_CODE,SCORE,RECORD_DATE) values (12,'admin',6,25,to_date('17/10/27','RR/MM/DD'));
---
+++ 기초데이터/tb_test.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-10월-27-2017   
+--------------------------------------------------------
+REM INSERTING into HR.TB_TEST
+SET DEFINE OFF;
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (21,'추가해본 회차',11,to_date('17/10/26','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (1,'배틀',0,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (2,'필드',1,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (3,'그라운드',1,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (4,'프론트',1,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (5,'4',2,to_date('13/10/29','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (6,'1',2,to_date('16/10/21','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (7,'얼리억세스',3,to_date('17/03/24','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (8,'1',4,to_date('15/11/17','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (9,'2',4,to_date('17/11/17','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (43,'1회',42,to_date('17/10/27','RR/MM/DD'));
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (10,'추가해본 시험',0,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (11,'추가해본 등급',10,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (41,'컴활',0,null);
+Insert into HR.TB_TEST (TEST_CODE,TEST_NAME,GROUP_CODE,TEST_DATE) values (42,'2급',41,null);
